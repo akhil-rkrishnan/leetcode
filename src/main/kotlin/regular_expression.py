a60e8b49205715624b0021ddbf0b3b074253031f
Akhil Krishnan <akhilrk19@gmail.com>
2022-02-08 16:42:01 +0530

    Add files via upload

--- src/main/kotlin/regular_expression.py
+++ src/main/kotlin/regular_expression.py
@@ -10,20 +10,22 @@ def isSatisfiedRegularExpression(p, s):
     passed = True
     if (p == ".*"):
         return passed
-    if (p.find("*") == -1):
+    if (p.find("*") == -1 and p.find(".") == -1):
         if (p != s):
             return not passed
-
+    c = ""
     while (k < pLen):
 
-        if (j < sLen and p[k] == s[j]):
+        if (j < sLen and (p[k] == s[j] or p[k] == ".")):
             k = k + 1; j = j + 1
         elif (p[k] == "*"):
             c = p[k-1]
             if (j == sLen):
-                break
+                k = k + 1
+                # break
+            
             while (j < sLen):
-                if (s[j] == c):
+                if (s[j] == c or c == "."):
                     j = j + 1
                 elif(k + 1 < pLen and (p[k + 1] == s[j] or p[k + 1] == ".")):
                     k = k + 2; j = j + 1; break
@@ -34,19 +36,34 @@ def isSatisfiedRegularExpression(p, s):
                     passed = False
                     k = pLen
                     break      
+           
         elif(k + 1 < pLen and p[k + 1] == "*"):
             k = k + 2
-            
-        # elif(k + 1 < pLen and p[k + 1] == "."):
-        #     k = k + 1
+        
+        elif (p[k] == c):
+            k = k + 1
+
         else:
             passed = False   
             print("IN-2")         
             break
-   
+
+    if (j != sLen):
+        passed = False
+        print("IN-3")
+
     return passed
-        
-string = "abcd"
-pattern = "f*"             
+
+string = "bbbba"
+pattern = ".*a*a"      
+
+# string = "aaa"
+# pattern = "a*a" 
+
+# string = "ab"
+# pattern = ".*c"  
+
+# string = "aa"
+# pattern = "a*"
 
 print(isSatisfiedRegularExpression(pattern, string))
